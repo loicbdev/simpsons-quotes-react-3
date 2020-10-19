d8bb8199892ddd47d0eb6540df5e9d0888df5434
loicbdev <70198199+loicbdev@users.noreply.github.com>
2020-10-19 12:10:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import QuoteList from './components/QuoteList';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <QuoteList />
+    </div>
+  );
+}
+export default App;
